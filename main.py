d2f7ce573d6fe43b1f2ed0a8afbde35698141ced
Timothy Chatman <tjchatman1986@gmail.com>
2023-07-06 10:56:04 -0400

    Basic additions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 try:
     import sys
     import tkinter as tk
+    import tk.tkFileDialog
 except ImportError as err:
     print(f'Could not load module. {err}')
     sys.exit(2)
@@ -8,6 +9,16 @@ except ImportError as err:
 main = tk.Tk()
 text = tk.Text(main)
 text.grid()
+
+def save_as():
+    global text
+    t = text.get("1.0", "end-1c")
+    save_location = tk.tkFileDialog.asksaveasfilename()
+    file1 = open(save_location, "w+")
+    file1.write(t)
+    files1.close()
+button = Button(root, text="Save", command=saveas)
+button.grid()
 
 main.mainloop()
 
